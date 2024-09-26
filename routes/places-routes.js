ceb0446baa9e4a05c8cab0002e9d696a6e7340f7
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-25 20:03:45 -0400

    Error Handling

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const HttpError = require('../models/http-error');
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -34,9 +34,7 @@ router.get("/:pid", (req, res, next) => {
   });
 
   if (!place) {
-    return res
-      .status(404)
-      .json({ message: "Could not find a place for the provided place id." });
+    throw new HttpError('Could not find a place for the provided place id.', 404);
   }
 
   res.json(place);
@@ -49,9 +47,7 @@ router.get("/user/:uid", (req, res, next) => {
   });
 
   if (!places || places.length === 0) {
-    return res
-      .status(404)
-      .json({ message: "Could not find a places for the provided user id." });
+    return next(new HttpError('Could not find a places for the provided user id.', 404)); 
   }
 
   res.json({ places });
