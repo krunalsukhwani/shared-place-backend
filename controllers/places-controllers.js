c2499fbe4b7cb996d36344472f3d68c8044a0207
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-25 20:53:22 -0400

    Added Controller

---
+++ controllers/places-controllers.js
@@ -0,0 +1,60 @@
+const HttpError = require('../models/http-error');
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Centennial College",
+    description: "One of the best college in GTA",
+    address: "941 Progress Ave, Scarborough, ON M1G 3T8",
+    location: {
+      lat: 43.7852043,
+      lng: -79.230744,
+    },
+    creator: "tyler",
+  },
+  {
+    id: "p2",
+    title: "Service Canada",
+    description: "Get all federal services from service canada",
+    address: "31 Tapscott Rd, Scarborough, ON M1B 4Y7",
+    location: {
+      lat: 43.7852024,
+      lng: -79.2487686,
+    },
+    creator: "nadzeya",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError(
+      "Could not find a place for the provided place id.",
+      404
+    );
+  }
+
+  res.json(place);
+};
+
+const getPlacesByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const places = DUMMY_PLACES.filter((p) => {
+    return p.creator === userId;
+  });
+
+  if (!places || places.length === 0) {
+    return next(
+      new HttpError("Could not find a places for the provided user id.", 404)
+    );
+  }
+
+  res.json({ places });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
