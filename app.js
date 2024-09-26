987a24d86d4bf851094de7ef5541bc8f9f53a71a
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-09-25 21:17:27 -0400

    Handled unsupported exception

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
+const HttpError = require('./models/http-error');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use('/api/places',placesRoutes);
+
+app.use((req, res, next) => {
+    throw new HttpError('Could not find the route.', 404);
+});
 
 //add middleware to handle the error
 app.use((error, req, res, next) => {
