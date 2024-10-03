10a913d8e31324088eaffa522a68021fe24310be
krunalsukhwani <krunal.sukhwani@gmail.com>
2024-10-02 21:18:50 -0400

    Get places by user id

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -53,11 +53,17 @@ const getPlaceById = async (req, res, next) => {
   res.json({place : place.toObject({ getters: true })});
 };
 
-const getPlacesByUserId = (req, res, next) => {
+const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  const places = DUMMY_PLACES.filter((p) => {
-    return p.creator === userId;
-  });
+  
+  let places;
+
+  try{
+    places = await Place.find({creator: userId});
+  }catch(err){
+    const error = new HttpError('Fetching places failed, try again later!', 500);
+    return next(error);
+  }
 
   if (!places || places.length === 0) {
     return next(
@@ -65,7 +71,7 @@ const getPlacesByUserId = (req, res, next) => {
     );
   }
 
-  res.json({ places });
+  res.json({ places: places.map(place => place.toObject({getters: true})) });
 };
 
 const createPlace = async (req, res, next) => {
